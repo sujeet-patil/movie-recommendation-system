09308a583fbda263791a417dbec06f1a72495263
sujeet-patil <patilsujeet46@gmail.com>
2024-02-08 23:36:00 +0530

    Enabled cross origin

--- src/main/java/com/sujeet/recommendations/controller/MovieController.java
+++ src/main/java/com/sujeet/recommendations/controller/MovieController.java
@@ -1,12 +1,10 @@
 package com.sujeet.recommendations.controller;
 
 import com.sujeet.recommendations.entity.request.SearchRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import com.sujeet.recommendations.entity.Movie;
@@ -16,6 +14,8 @@ import com.sujeet.recommendations.service.MovieService;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
+@Slf4j
 public class MovieController {
     @Autowired
     private MovieService movieService;
